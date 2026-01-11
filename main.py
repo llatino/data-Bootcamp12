3b04921df34abde305128a6810456d1fce69f0ca
llatino <tankawee.boonpeng@gmail.com>
2026-01-11 12:39:41 +0700

    Update main.py
    Now i know

--- main.py
+++ main.py
@@ -1,12 +1,2 @@
 #print("hello world")
-print("hello world")
-print("good bye")
-
-#print remote
-print("i'm leaning github")
-
-# new function
-def add_two(num):
-    return num + 2
-
-print(add_two(2),add_two(5))
+print("Now I know ")
